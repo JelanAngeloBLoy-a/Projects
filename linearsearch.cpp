0b65db259a92488579292b957331e1f2c13ac69e
JelanAngeloBLoy-a <qjaloy-a01@tip.edu.ph>
2025-09-10 23:21:18 +0800

    Create linearsearch.cpp

---
+++ linearsearch.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int Data[]= {5, 3, 8, 4, 2};
+	int Item;
+	int N = sizeof(Data) / sizeof(Data[0]);
+	int I=0;
+
+	cout<<"Insert an item to find: ";
+	cin>>Item;
+	while(I<=N){
+	    if(Item==Data[I]){
+	        cout<<"Searching is Successful"<<endl;
+	        break;
+	    }
+	    
+	    else{
+	        cout<<"Searching is Unsuccessful"<<endl;
+	        
+	    }
+	    I++;
+	}
+	return 0;
+}
