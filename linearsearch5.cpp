a5d54c05f318042fe2be672f9e81acd4519b4972
JelanAngeloBLoy-a <qjaloy-a01@tip.edu.ph>
2025-09-11 01:13:08 +0800

    Create linearsearch5.cpp

---
+++ linearsearch5.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int Data[]= {12, 46, 16, 15, 18, 55, 20, 72, 13, 8, 23};
+	int Item;
+	int N = sizeof(Data) / sizeof(Data[0]);
+	int I=0;
+
+	cout<<"Insert an item to find: ";
+	cin>>Item;
+	while(I<=N){
+	    if(Item==Data[I]){
+	        cout<<"Searching is Successful"<<endl;
+	        cout<<"Found at index:"<<"["<<I<<"]"<<endl;
+	        break;
+	    }
+	    
+	    else{
+	        cout<<"it is not existing"<<endl;
+	        
+	    }
+	    I++;
+	}
+	return 0;
+}
